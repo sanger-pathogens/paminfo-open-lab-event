4da64c34f3fe7fea5a6934c25d4f6b975426ad27
Elisabetta Galatola <eg19@sanger.ac.uk>
2024-02-07 14:26:43 +0000

    Amend solutions

--- src/routes/solutions/+page.svelte
+++ src/routes/solutions/+page.svelte
@@ -7,41 +7,31 @@
     // template literal
     alert(`Hello ${text}`);
   }
-
-  let questions = [
-    { id: 1, text: `Where did you go to school?` },
-    { id: 2, text: `What is your mother's name?` },
-    {
-      id: 3,
-      text: `What is another personal fact that an attacker could easily find with Google?`,
-    },
-  ];
-
-  let selected;
-
-  let answer = "";
-
-  function handleSubmit() {
-    alert(`You answered to question ${selected.id}:
-    Q: ${selected.text} 
-    A: "${answer}"`);
-  }
 </script>
 
 <h2>Solutions :)</h2>
 
+<!-- EXERCISE ONE -->
 <div>
   1. Onclick event
   <h5>
-    <li>Create a button which displays a "Hello World" message each time the button
-    gets clicked (either as via console log or alert)<br /></li>
-    <li>Change the style of
-    your button: e.g. make the background black/the text white</li>
+    <li>
+      Create a button which displays a "Hello World" message each time the
+      button gets clicked (either as via console log or alert)<br />
+    </li>
+    <li>
+      Change the style of your button: e.g. make the background black/the text
+      white
+    </li>
   </h5>
   <h3>
     <button on:click={helloAlert}>Button</button>
   </h3>
   <br />
+  <!-- END -->
+
+  <!-- EXERCISE TWO -->
+
   2. Fix the bug<br />
   <h5>
     A 'file_format' object is being imported from `./constant_store.svelte`, but
@@ -54,6 +44,9 @@
     </h4>
   </h5>
 
+  <!-- END -->
+
+  <!-- EXERCISE THREE -->
   <div>
     3. Create and Populate a table
 
@@ -61,9 +54,6 @@
       Using the same imported `file_format` object, display all of its property
       in a table
     </h5>
-
-    <!-- HINT: use a simple HTML table structure
-            a table style is already provided at the bottom of this file -->
 
     <table>
       <tr>
@@ -83,45 +73,10 @@
   </div>
 </div>
 
-<!-- 
-<div>
-<h2>Option box</h2>
-
-<form on:submit|preventDefault={handleSubmit}>
-	<select bind:value={selected} on:change={() => (answer = '')}>
-		{#each questions as question}
-			<option value={question}>
-				{question.text}
-			</option>
-		{/each}
-	</select>
-
-	<input bind:value={answer} />
-
-	<button disabled={!answer} type="submit"> Submit </button>
-</form>
-
-<p>selected question {selected ? selected.id : '[waiting...]'}</p>
-</div>
- -->
+<!-- END -->
 <style>
   button {
     background-color: rgb(0, 0, 0);
     color: white;
-  }
-
-  table,
-  th,
-  td {
-    max-width: 60rem;
-    border: 1px solid;
-    border-collapse: collapse;
-    margin-bottom: 10px;
-    align-content: center;
-  }
-  input {
-    display: block;
-    width: 500px;
-    max-width: 100%;
   }
 </style>
