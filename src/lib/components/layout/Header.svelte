7d7c6d181f359f650b2942f5bd5975ee84619087
Elisabetta Galatola <eg19@sanger.ac.uk>
2024-02-07 14:27:02 +0000

    Amend header

--- src/lib/components/layout/Header.svelte
+++ src/lib/components/layout/Header.svelte
@@ -45,30 +45,4 @@
     max-width: 15rem;
     margin-top: 1.9rem;
   }
-
-  @media (max-width: 1000px) {
-    h1 {
-      left: 32%;
-      margin-top: 0.4rem;
-    }
-
-    .pam_logo {
-      padding: 0.5rem;
-      transform: scale(0.7);
-    }
-
-    nav {
-      min-width: 44rem;
-    }
-  }
-
-  @media (max-width: 550px) {
-    h1 {
-      left: 40%;
-    }
-
-    nav {
-      min-width: 36rem;
-    }
-  }
 </style>
