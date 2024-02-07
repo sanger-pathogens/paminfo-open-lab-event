8e0266b49c617f767188a169d02f361a24368d50
Elisabetta Galatola <eg19@sanger.ac.uk>
2024-02-07 14:25:15 +0000

    remove files

--- .svelte-kit/output/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.5d57774e.js","_app/immutable/chunks/index.7db445bd.js"];
-export const stylesheets = ["_app/immutable/assets/0.348658bb.css"];
-export const fonts = [];
--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.e7985819.js","_app/immutable/chunks/index.7db445bd.js","_app/immutable/chunks/singletons.ac08fd34.js"];
-export const stylesheets = [];
-export const fonts = [];
